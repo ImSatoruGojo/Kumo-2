aba5a069c69d4215440eaed08fd7d4c71255e110
Satoru <yashyadav2036@gmail.com>
2026-09-25 16:19:32 +0530

    Add runtime catalog store

---
+++ app/src/main/java/app/kumo/beta/data/CatalogStore.kt
@@ -0,0 +1,11 @@
+package app.kumo.beta.data
+
+import app.kumo.beta.model.Title
+import java.util.concurrent.ConcurrentHashMap
+
+object CatalogStore {
+    private val titles = ConcurrentHashMap<String, Title>()
+    fun put(title: Title) { titles[title.id] = title }
+    fun putAll(items: Iterable<Title>) { items.forEach(::put) }
+    fun get(id: String): Title? = titles[id]
+}
